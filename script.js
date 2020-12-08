2115288f039bb184ff2641e1ad79c24cadb2242e
Peter Gemmell <peter.gemmell@ocsbstudent.ca>
2020-12-08 15:46:25 +0000

    ffibfawfbaiufbauaifvba

--- script.js
+++ script.js
@@ -1,4 +1,3 @@
-document.getElementById("textbox").addEventListener("click", function() {
-  alert("Chameleons or chamaeleons are a distinctive and highly specialized clade of Old World lizards with 202 species described as of June 2015. These species come in a range of colors, and many species have the ability to change color.");
-});
-
+document.getElementById('textbox').addEventListener('click', function () {
+  alert('Chameleons or chamaeleons are a distinctive and highly specialized clade of Old World lizards with 202 species described as of June 2015. These species come in a range of colors, and many species have the ability to change color.')
+})
